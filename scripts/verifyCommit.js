069f887a82190defe27be41e1ff7e1ed2bd612db
yanxianping <yanxianping@haodf.com>
2020-11-10 20:13:13 +0800

    chore: add commit-convention.md and change commit-msg vefify

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -8,14 +8,14 @@ const chalk = require('chalk');
 const fs = require('fs');
 
 const commitMsg = fs.readFileSync(process.env.HUSKY_GIT_PARAMS, 'utf-8').trim();
-const commitRegExp = /^(revert: )?(feat|fix|docs|style|test|perf|refactor|ci|chore|wip|build|release)(\(.+\))?: .{1,50}/;
+const commitRegExp = /^(revert: )?(feat|fix|docs|style|test|perf|refactor|ci|chore|wip|build|release)(\(.+\))?: .{1,60}/;
 
 if (!commitRegExp.test(commitMsg)) {
     console.error(`
         ${chalk.bgRed.white(' ERROR ')}${chalk.red('invalid commit message format.')}\n
         ${chalk.red(' Proper commit message format is required for automated changelog generation. For examples:')}\n
         ${chalk.green('     feat(toast): add "comments" options')}\n
-        ${chalk.red(' See .github/commit-convention.md for more details.')}\n
+        ${chalk.red(' See https://github.com/npm-uis/yui/tree/main/.github/commit-convention.md for more details.')}\n
     `);
     process.exit(1);
 }
