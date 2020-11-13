a2d299c2b85055bd022e957b3c6bce091be17a16
yanxianping <2378149796@qq.com>
2020-11-13 13:56:23 +0800

    wip(switch): base function

--- packages/switch/src/index.vue
+++ packages/switch/src/index.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class="y-switch">{{index}}</div>
+    <div
+        class="y-switch"
+        :style="{backgroundColor:value ? activeColor:inactiveColor}"
+        @click.stop="switchValue"
+    >
+        <span class="y-switch-slider" :class="[value ? 'y-switch-slider-active': 'y-switch-slider-inactive']"></span>
+    </div>
 </template>
-
 <script>
     import Switch from './js/index';
 
     export default Switch;
 </script>
-
 <style lang="scss" scoped>
     @import "./css/index.scss";
 </style>
--- packages/switch/src/js/index.js
+++ packages/switch/src/js/index.js
@@ -1,8 +1,39 @@
 export default {
     name: 'ySwitch',
+    model: {
+        prop: 'value',
+        event: 'input'
+    },
+    props: {
+        // status
+        value: {
+            type: Boolean,
+            default: true
+        },
+        // active color when status true
+        activeColor: {
+            type: String,
+            default: '#409eff'
+        },
+        // inactive color when status false
+        inactiveColor: {
+            type: String,
+            default: '#c0ccda'
+        }
+    },
     data() {
         return {
             index: 123
+        }
+    },
+    methods: {
+        switchValue() {
+            this.value = !this.value;
+            this.handleChange();
+        },
+        handleChange() {
+            this.$emit('change', this.value);
+            this.$emit('input', this.value);
         }
     }
 }
